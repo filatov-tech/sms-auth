ff6ed53b3a07529492dc428b3a3f24bd8bb1317b
filatov <58160752+filatov-dd@users.noreply.github.com>
2024-02-12 23:35:12 -0300

    Add the liquibase files

---
+++ src/main/resources/db/changelog/changeset/populate-users-table-with-test-data.sql
@@ -0,0 +1,5 @@
+-- changeset filatov-tech:2
+INSERT INTO users (id, firstname, lastname, phone_number, password, role)
+VALUES
+    ('70350b52-375d-4921-921e-2c7b85a1abd4', 'Terminator', 'T-800', 7777777, '$2a$12$LvZ.DuBCrST1A/QK4fOuuuBbC0uCdaZPE3mGGHWRyMjv0UovuInVu', 'ADMIN'),
+    ('68cd6329-d953-448e-ba6c-52d0536afebc', 'Terminator', 'T-1000', 4444, '$2a$12$wiE.t5p1VGerV3pade5K2Op.8wg9/BysY7uZwe1RL5nBOIN7Vh5I.', 'USER')
